89854b29956bc7177c056530fd1939184657cb37
Abhishek Yadav <ydvabhee@users.noreply.github.com>
2020-07-10 14:31:31 +0530

    update navbar

--- app-min.js
+++ app-min.js
@@ -1,18 +1,67 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+const navCon = document.getElementById('navCon');
+const navLinks = document.getElementById('navLinks');
+
+function addNavCon(event) {
+
+    let x = window.matchMedia("(max-width: 920px)")
+    if (x.matches) { // If media query matches
+        if (navCon.style.height == '100vh') {
+            event.classList.remove("active");
+            navCon.style.position = "fixed";
+            return navCon.style.height = '0em';
+
+        }
+
+        console.log(event);
+        navCon.style.height = '100vh';
+        event.classList.add("active");
+        navCon.style.position = "absolute";
+        return;
     }
+
+
+
+    if (navCon.style.height == '30em' || navCon.style.height == '100vh') {
+        event.classList.toggle("active");
+        navCon.style.position = "absolute";
+        return navCon.style.height = '0em';
+
+    }
+
+    console.log(event);
+    navCon.style.height = '30em';
+    event.classList.toggle("active");
+    // navCon.style.position = "relative";
+
+    // console.log(navCon);
+
 }
 
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
+function viewLinks(event) {
+
+    if (navLinks.style.transform == 'scale(1)') {
+        navLinks.style.transform = 'scale(0)';
+        navCon.style.height = '0em';
+        document.body.getElementsByClassName("active")[0].classList.remove("active");
+        return
+    }
+    navLinks.style.transform = 'scale(1)';
 }
 
-/* Close */
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-}
+const showNavLinks = () => {
+    let x = window.innerWidth;
+    if (x > 840) {
+        navLinks.style.transform = 'scale(1)';
+        document.body.getElementsByClassName("active").classList.remove('active');
+
+        navCon.style.height = '0em';
+    } else
+        navLinks.style.transform = 'scale(0)';
+    document.body.getElementsByClassName("active")[0].classList.remove("active");
+    navCon.style.height = '0em';
+
+
+};
+
+
+window.addEventListener('resize', showNavLinks);
