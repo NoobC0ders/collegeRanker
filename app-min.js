1ef32d53d32fa06b357bfda509815558c4128274
Abhishek Yadav <ydvabhee@users.noreply.github.com>
2020-07-11 20:09:55 +0530

    nav update

--- app-min.js
+++ app-min.js
@@ -8,35 +8,33 @@ function addNavCon(event) {
         if (navCon.style.height == '100vh') {
             event.classList.remove("active");
             navCon.style.position = "fixed";
+            document.getElementById('dropdownicon').style.transform = 'rotateX(0deg)';
             return navCon.style.height = '0em';
-
         }
 
         console.log(event);
+        document.getElementById('dropdownicon').style.transform = 'rotateX(180deg)';
         navCon.style.height = '100vh';
         event.classList.add("active");
         navCon.style.position = "absolute";
         return;
     }
 
-
+    /***************************************************************/
 
     if (navCon.style.height == '30em' || navCon.style.height == '100vh') {
-        event.classList.toggle("active");
+
+        document.getElementById('dropdownicon').style.transform = 'rotateZ(0deg)';
         navCon.style.position = "absolute";
         return navCon.style.height = '0em';
 
     }
+    document.getElementById('dropdownicon').style.transform = 'rotateZ(180deg)';
 
-    console.log(event);
     navCon.style.height = '30em';
-    event.classList.toggle("active");
-    // navCon.style.position = "relative";
-
-    // console.log(navCon);
 
 }
-
+/****************************************************************/
 function viewLinks(event) {
 
     if (navLinks.style.transform == 'scale(1)') {
@@ -51,17 +49,67 @@ function viewLinks(event) {
 const showNavLinks = () => {
     let x = window.innerWidth;
     if (x > 840) {
+        document.getElementById('dropdownicon').style.transform = 'rotateX(0deg)';
+
         navLinks.style.transform = 'scale(1)';
-        document.body.getElementsByClassName("active").classList.remove('active');
+        // document.body.getElementsByClassName("active").classList.remove('active');
 
         navCon.style.height = '0em';
-    } else
+    } else {
         navLinks.style.transform = 'scale(0)';
-    document.body.getElementsByClassName("active")[0].classList.remove("active");
-    navCon.style.height = '0em';
-
+        // document.body.getElementsByClassName("active")[0].classList.remove("active");
+        navCon.style.height = '0em';
+    }
 
 };
+
+
+
+function showDegree(element, {
+    expand
+}) {
+
+
+    //close engg degree
+    document.getElementById('engg').setAttribute("onclick", 'showDegree(this,{expand:false})');
+    document.getElementById('engg').children[0].style.transform = 'rotateZ(0deg)';
+    document.getElementById('engg').children[1].style.display = 'none';
+
+    //close medical degree
+    document.getElementById('medical').setAttribute("onclick", 'showDegree(this,{expand:false})');
+    document.getElementById('medical').children[0].style.transform = 'rotateZ(0deg)';
+    document.getElementById('medical').children[1].style.display = 'none';
+
+    //close engg degree
+    document.getElementById('management').setAttribute("onclick", 'showDegree(this,{expand:false})');
+    document.getElementById('management').children[0].style.transform = 'rotateZ(0deg)';
+    document.getElementById('management').children[1].style.display = 'none';
+
+    //close engg degree
+    document.getElementById('commerce').setAttribute("onclick", 'showDegree(this,{expand:false})');
+    document.getElementById('commerce').children[0].style.transform = 'rotateZ(0deg)';
+    document.getElementById('commerce').children[1].style.display = 'none';
+
+    //close engg degree
+    document.getElementById('art').setAttribute("onclick", 'showDegree(this,{expand:false})');
+    document.getElementById('art').children[0].style.transform = 'rotateZ(0deg)';
+    document.getElementById('art').children[1].style.display = 'none';
+
+    if (expand) {
+
+        element.setAttribute("onclick", 'showDegree(this,{expand:false})');
+        console.log(element.children[0].style.transform = 'rotateZ(0deg)');
+        element.children[1].style.display = 'none';
+
+    } else {
+        console.log(element.children[0].style.transform = 'rotateZ(90deg)');
+        element.setAttribute("onclick", 'showDegree(this,{expand:true})');
+        element.children[1].style.display = 'block';
+
+    }
+
+}
+
 
 
 window.addEventListener('resize', showNavLinks);
